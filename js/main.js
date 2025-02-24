b4698acf28812e947ef68ed02a2ad6e70e71b40a
maruf010 <mhbijoy013@gmail.com>
2025-02-25 04:30:00 +0600

    cash out added

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
 document.getElementById('add-money').addEventListener('click',function(event){
     event.preventDefault();
-    const addAmount = document.getElementById('add-amount').value;
-    const convertedAmount = parseFloat(addAmount);
+
     const pin = document.getElementById('pinNumber').value;
     const pinNew = parseInt(pin)
+
+    const addAmount = document.getElementById('add-amount').value;
+    const convertedAmount = parseFloat(addAmount);
     
     const mainBalance = document.getElementById('main-balance').innerText;
     const addMainBalance = parseFloat(mainBalance);
 
-    if(pinNew === 1234){
-        const sum = convertedAmount + addMainBalance;
-        document.getElementById('main-balance').innerText = sum;
+    if(addAmount && pin){
+        if(pinNew === 1234){
+            const sum = convertedAmount + addMainBalance;
+            document.getElementById('main-balance').innerText = sum;
+        }
+        else{
+            alert("Incorrect Pin! Please Type Correct Pin.");
+        }
     }
     else{
-        console.log("Wrong Pin");
+        alert('Fill-up the all input Please')
     }
 })
